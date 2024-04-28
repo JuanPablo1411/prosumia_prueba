ab9a4b9c28dce0a29ee7b74f382957eb78048c03
Juan <juampi.mantegazza@gmail.com>
2024-04-28 19:38:24 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [heights, setHeights] = useState("");
+  const [waterTrapped, setWaterTrapped] = useState(null);
+
+  const calculateWaterTrapped = () => {
+    const heightsArray = heights.split(" ").map(Number);
+    let left = 0,
+      right = heightsArray.length - 1;
+    let leftMax = 0,
+      rightMax = 0;
+    let water = 0;
+
+    while (left < right) {
+      if (heightsArray[left] < heightsArray[right]) {
+        if (heightsArray[left] >= leftMax) {
+          leftMax = heightsArray[left];
+        } else {
+          water += leftMax - heightsArray[left];
+        }
+        left += 1;
+      } else {
+        if (heightsArray[right] >= rightMax) {
+          rightMax = heightsArray[right];
+        } else {
+          water += rightMax - heightsArray[right];
+        }
+        right -= 1;
+      }
+    }
+
+    setWaterTrapped(water);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="left-section">
+        <div className="logo">
+          <img
+            className="logo-img"
+            src="/logo_prosumia.png"
+            alt="Company Logo"
+          />
+        </div>
+        {/* ... Resto del contenido izquierdo, incluyendo el cálculo del agua atrapada */}
+      </div>
+      <div className="right-section">
+        <div style={{ padding: "20px" }}>
+          <div className="container">
+            <h1>Water Trapped Calculator</h1>
+            <label>
+              Enter heights (separated by space):
+              <input
+                className="input-field"
+                type="text"
+                value={heights}
+                onChange={(e) => setHeights(e.target.value)} // Seteo la variable heights desde el input.
+              />
+            </label>
+            <button
+              className="calculate-button"
+              onClick={calculateWaterTrapped}
+            >
+              Calculate
+            </button>
+          </div>
+          {waterTrapped !== null && (
+            <div>
+              <h2>Water Trapped: {waterTrapped} units</h2>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
